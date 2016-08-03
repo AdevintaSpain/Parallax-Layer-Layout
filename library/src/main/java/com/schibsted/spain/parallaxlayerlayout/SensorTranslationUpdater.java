2ad699cede37550b1a545aed95e5ddee75ec7cac
Rafa Vázquez <rafa.vazsan@gmail.com>
2016-08-03 16:40:06 +0200

    Applied new formatting and fixed errors
    Reformatted the project with the new code style, and fixed all the problems found by Checkstyle and PMD.

--- library/src/main/java/com/schibsted/spain/parallaxlayerlayout/SensorTranslationUpdater.java
+++ library/src/main/java/com/schibsted/spain/parallaxlayerlayout/SensorTranslationUpdater.java
@@ -10,179 +10,186 @@ import android.support.annotation.Nullable;
 import android.view.Surface;
 import android.view.WindowManager;
 
-public class SensorTranslationUpdater implements ParallaxLayerLayout.TranslationUpdater, SensorEventListener {
+public class SensorTranslationUpdater
+    implements ParallaxLayerLayout.TranslationUpdater, SensorEventListener {
 
-    private static final int DEFAULT_SAMPLING_PERIOD = 100;
+  private static final int DEFAULT_SAMPLING_PERIOD = 100;
+  private final SensorManager sensorManager;
+  private float[] mTiltVector = new float[3];
+  private boolean mTargeted = false;
+  private float[] mTargetMatrix = new float[16];
+  private float[] mRotationMatrix = new float[16];
+  private float[] mOrientedRotationMatrix = new float[16];
+  private float[] mTruncatedRotationVector;
+  private float mTiltSensitivity = 2.0f;
+  private ParallaxLayerLayout parallax;
 
-    private float[] mTiltVector = new float[3];
-    private boolean mTargeted = false;
-    private float[] mTargetMatrix = new float[16];
-    private float[] mRotationMatrix = new float[16];
-    private float[] mOrientedRotationMatrix = new float[16];
-    private float[] mTruncatedRotationVector;
-    private float mTiltSensitivity = 2.0f;
+  public SensorTranslationUpdater(Context context) {
+    this((SensorManager) context.getSystemService(Context.SENSOR_SERVICE));
+  }
 
+  public SensorTranslationUpdater(SensorManager sensorManager) {
+    this.sensorManager = sensorManager;
+  }
 
-    private final SensorManager sensorManager;
-    private ParallaxLayerLayout parallax;
+  @Override
+  public void subscribe(ParallaxLayerLayout parallaxLayerLayout) {
+    parallax = parallaxLayerLayout;
+  }
 
-    public SensorTranslationUpdater(Context context) {
-        this((SensorManager) context.getSystemService(Context.SENSOR_SERVICE));
+  @Override
+  public void unSubscribe() {
+    parallax = null;
+  }
+
+  public void registerSensorManager() {
+    if (sensorManager != null) {
+      sensorManager.registerListener(this,
+          sensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR), DEFAULT_SAMPLING_PERIOD);
+    }
+  }
+
+  public void unregisterSensorManager() {
+    if (sensorManager != null) {
+      sensorManager.unregisterListener(this);
+    }
+  }
+
+  @Override
+  public void onSensorChanged(SensorEvent event) {
+    if (parallax == null) {
+      return;
     }
 
-    public SensorTranslationUpdater(SensorManager sensorManager) {
-        this.sensorManager = sensorManager;
+    final float[] vectors = interpretSensorEvent(parallax.getContext(), event);
+    if (vectors == null) {
+      return;
     }
 
-    @Override
-    public void subscribe(ParallaxLayerLayout parallaxLayerLayout) {
-        parallax = parallaxLayerLayout;
+    float roll = vectors[2];
+    float pitch = -vectors[1];
+
+    parallax.updateTranslations(new float[] { roll, pitch });
+  }
+
+  @Override
+  public void onAccuracyChanged(Sensor sensor, int accuracy) {
+    //NA
+  }
+
+  @Nullable
+  @SuppressWarnings("SuspiciousNameCombination")
+  private float[] interpretSensorEvent(@NonNull Context context, @Nullable SensorEvent event) {
+    if (event == null) {
+      return null;
     }
 
-    @Override
-    public void unSubscribe() {
-        parallax = null;
+    // Retrieves the RotationVector from SensorEvent
+    float[] rotationVector = getRotationVectorFromSensorEvent(event);
+
+    // Set target rotation if none has been set
+    if (!mTargeted) {
+      setTargetVector(rotationVector);
+      return null;
     }
 
-    public void registerSensorManager() {
-        if (sensorManager != null) {
-            sensorManager.registerListener(this, sensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR), DEFAULT_SAMPLING_PERIOD);
-        }
+    // Get rotation matrix from event's values
+    SensorManager.getRotationMatrixFromVector(mRotationMatrix, rotationVector);
+
+    // Acquire rotation of screen
+    final int rotation =
+        ((WindowManager) context.getSystemService(Context.WINDOW_SERVICE)).getDefaultDisplay()
+            .getRotation();
+
+    // Calculate angle differential between target and current orientation
+    if (rotation == Surface.ROTATION_0) {
+      SensorManager.getAngleChange(mTiltVector, mRotationMatrix, mTargetMatrix);
+    } else {
+      // Adjust axes on screen orientation by remapping coordinates
+      switch (rotation) {
+        case Surface.ROTATION_90:
+          SensorManager.remapCoordinateSystem(mRotationMatrix, SensorManager.AXIS_Y,
+              SensorManager.AXIS_MINUS_X, mOrientedRotationMatrix);
+          break;
+
+        case Surface.ROTATION_180:
+          SensorManager.remapCoordinateSystem(mRotationMatrix, SensorManager.AXIS_MINUS_X,
+              SensorManager.AXIS_MINUS_Y, mOrientedRotationMatrix);
+          break;
+
+        case Surface.ROTATION_270:
+          SensorManager.remapCoordinateSystem(mRotationMatrix, SensorManager.AXIS_MINUS_Y,
+              SensorManager.AXIS_X, mOrientedRotationMatrix);
+          break;
+        default:
+          break;
+      }
+
+      SensorManager.getAngleChange(mTiltVector, mOrientedRotationMatrix, mTargetMatrix);
     }
 
-    public void unregisterSensorManager() {
-        if (sensorManager != null) {
-            sensorManager.unregisterListener(this);
-        }
+    // Perform value scaling and clamping on value array
+    for (int i = 0; i < mTiltVector.length; i++) {
+      // Map domain of tilt vector from radian (-PI, PI) to fraction (-1, 1)
+      mTiltVector[i] /= Math.PI;
+
+      // Adjust for tilt sensitivity
+      mTiltVector[i] *= mTiltSensitivity;
+
+      // Clamp values to bounds
+      if (mTiltVector[i] > 1) {
+        mTiltVector[i] = 1f;
+      } else if (mTiltVector[i] < -1) {
+        mTiltVector[i] = -1f;
+      }
     }
 
-    @Override
-    public void onSensorChanged(SensorEvent event) {
-        if (parallax == null) return;
+    return mTiltVector;
+  }
 
-        final float[] vectors = interpretSensorEvent(parallax.getContext(), event);
-        if (vectors == null) return;
+  @NonNull
+  private float[] getRotationVectorFromSensorEvent(@NonNull SensorEvent event) {
+    if (event.values.length > 4) {
+      // On some Samsung devices SensorManager.getRotationMatrixFromVector
+      // appears to throw an exception if rotation vector has length > 4.
+      // For the purposes of this class the first 4 values of the
+      // rotation vector are sufficient (see crbug.com/335298 for details).
+      if (mTruncatedRotationVector == null) {
+        mTruncatedRotationVector = new float[4];
+      }
+      System.arraycopy(event.values, 0, mTruncatedRotationVector, 0, 4);
+      return mTruncatedRotationVector;
+    } else {
+      return event.values;
+    }
+  }
 
-        float roll = vectors[2];
-        float pitch = -vectors[1];
+  /**
+   * Sets the target direction used for angle deltas to determine tilt.
+   *
+   * @param values a rotation vector (presumably from a ROTATION_VECTOR sensor)
+   */
+  private void setTargetVector(float[] values) {
+    SensorManager.getRotationMatrixFromVector(mTargetMatrix, values);
+    mTargeted = true;
+  }
 
-        parallax.updateTranslations(new float[]{roll, pitch});
+  public void reset() {
+    mTargeted = false;
+  }
+
+  /**
+   * Sets the new sensitivity that will scale tilt calculations by. If this
+   * sensitivity is above 1, the interpreter will have to clamp percentages to 100% and -100% at
+   * the tilt extremes.
+   *
+   * @param tiltSensitivity the new tilt sensitivity
+   */
+  public void setTiltSensitivity(float tiltSensitivity) {
+    if (tiltSensitivity <= 0) {
+      throw new IllegalArgumentException("Tilt sensitivity must be positive");
     }
 
-    @Override
-    public void onAccuracyChanged(Sensor sensor, int accuracy) {
-        //NA
-    }
-
-    @Nullable
-    @SuppressWarnings("SuspiciousNameCombination")
-    private float[] interpretSensorEvent(@NonNull Context context, @Nullable SensorEvent event) {
-        if (event == null) {
-            return null;
-        }
-
-        // Retrieves the RotationVector from SensorEvent
-        float[] rotationVector = getRotationVectorFromSensorEvent(event);
-
-        // Set target rotation if none has been set
-        if (!mTargeted) {
-            setTargetVector(rotationVector);
-            return null;
-        }
-
-        // Get rotation matrix from event's values
-        SensorManager.getRotationMatrixFromVector(mRotationMatrix, rotationVector);
-
-        // Acquire rotation of screen
-        final int rotation = ((WindowManager) context
-          .getSystemService(Context.WINDOW_SERVICE))
-          .getDefaultDisplay()
-          .getRotation();
-
-        // Calculate angle differential between target and current orientation
-        if (rotation == Surface.ROTATION_0) {
-            SensorManager.getAngleChange(mTiltVector, mRotationMatrix, mTargetMatrix);
-        } else {
-            // Adjust axes on screen orientation by remapping coordinates
-            switch (rotation) {
-                case Surface.ROTATION_90:
-                    SensorManager.remapCoordinateSystem(mRotationMatrix, SensorManager.AXIS_Y, SensorManager.AXIS_MINUS_X, mOrientedRotationMatrix);
-                    break;
-
-                case Surface.ROTATION_180:
-                    SensorManager.remapCoordinateSystem(mRotationMatrix, SensorManager.AXIS_MINUS_X, SensorManager.AXIS_MINUS_Y, mOrientedRotationMatrix);
-                    break;
-
-                case Surface.ROTATION_270:
-                    SensorManager.remapCoordinateSystem(mRotationMatrix, SensorManager.AXIS_MINUS_Y, SensorManager.AXIS_X, mOrientedRotationMatrix);
-                    break;
-            }
-
-            SensorManager.getAngleChange(mTiltVector, mOrientedRotationMatrix, mTargetMatrix);
-        }
-
-        // Perform value scaling and clamping on value array
-        for (int i = 0; i < mTiltVector.length; i++) {
-            // Map domain of tilt vector from radian (-PI, PI) to fraction (-1, 1)
-            mTiltVector[i] /= Math.PI;
-
-            // Adjust for tilt sensitivity
-            mTiltVector[i] *= mTiltSensitivity;
-
-            // Clamp values to bounds
-            if (mTiltVector[i] > 1) {
-                mTiltVector[i] = 1f;
-            } else if (mTiltVector[i] < -1) {
-                mTiltVector[i] = -1f;
-            }
-        }
-
-        return mTiltVector;
-    }
-
-    @NonNull
-    private float[] getRotationVectorFromSensorEvent(@NonNull SensorEvent event) {
-        if (event.values.length > 4) {
-            // On some Samsung devices SensorManager.getRotationMatrixFromVector
-            // appears to throw an exception if rotation vector has length > 4.
-            // For the purposes of this class the first 4 values of the
-            // rotation vector are sufficient (see crbug.com/335298 for details).
-            if (mTruncatedRotationVector == null) {
-                mTruncatedRotationVector = new float[4];
-            }
-            System.arraycopy(event.values, 0, mTruncatedRotationVector, 0, 4);
-            return mTruncatedRotationVector;
-        } else {
-            return event.values;
-        }
-    }
-
-    /**
-     * Sets the target direction used for angle deltas to determine tilt.
-     *
-     * @param values a rotation vector (presumably from a ROTATION_VECTOR sensor)
-     */
-    private void setTargetVector(float[] values) {
-        SensorManager.getRotationMatrixFromVector(mTargetMatrix, values);
-        mTargeted = true;
-    }
-
-    public void reset() {
-        mTargeted = false;
-    }
-
-    /**
-     * Sets the new sensitivity that will scale tilt calculations by. If this
-     * sensitivity is above 1, the interpreter will have to clamp percentages to 100% and -100% at
-     * the tilt extremes.
-     *
-     * @param tiltSensitivity the new tilt sensitivity
-     */
-    public void setTiltSensitivity(float tiltSensitivity) {
-        if (tiltSensitivity <= 0) {
-            throw new IllegalArgumentException("Tilt sensitivity must be positive");
-        }
-
-        mTiltSensitivity = tiltSensitivity;
-    }
+    mTiltSensitivity = tiltSensitivity;
+  }
 }
--- sample/src/main/java/com/schibsted/spain/parallaxlayerlayout/sample/MainActivity.java
+++ sample/src/main/java/com/schibsted/spain/parallaxlayerlayout/sample/MainActivity.java
@@ -4,59 +4,58 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.RadioGroup;
-
 import com.schibsted.spain.parallaxlayerlayout.AnimatedTranslationUpdater;
 import com.schibsted.spain.parallaxlayerlayout.ParallaxLayerLayout;
 import com.schibsted.spain.parallaxlayerlayout.SensorTranslationUpdater;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ParallaxLayerLayout parallaxLayout;
-    private SensorTranslationUpdater translationUpdater;
+  private ParallaxLayerLayout parallaxLayout;
+  private SensorTranslationUpdater translationUpdater;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_ovni);
-        //setContentView(R.layout.activity_main_squares);
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main_ovni);
+    //setContentView(R.layout.activity_main_squares);
 
-        parallaxLayout = (ParallaxLayerLayout) findViewById(R.id.parallax);
-        RadioGroup updaterGroup = (RadioGroup) findViewById(R.id.updater_group);
+    parallaxLayout = (ParallaxLayerLayout) findViewById(R.id.parallax);
+    RadioGroup updaterGroup = (RadioGroup) findViewById(R.id.updater_group);
 
-        translationUpdater = new SensorTranslationUpdater(this);
+    translationUpdater = new SensorTranslationUpdater(this);
 
-        //noinspection ConstantConditions
-        updaterGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(RadioGroup group, int checkedId) {
-                if (checkedId == R.id.updater_sensor_button) {
-                    parallaxLayout.setTranslationUpdater(translationUpdater);
-                } else if (checkedId == R.id.updater_auto_button) {
-                    parallaxLayout.setTranslationUpdater(new AnimatedTranslationUpdater(0.5f));
-                }
-            }
-        });
+    //noinspection ConstantConditions
+    updaterGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+      @Override
+      public void onCheckedChanged(RadioGroup group, int checkedId) {
+        if (checkedId == R.id.updater_sensor_button) {
+          parallaxLayout.setTranslationUpdater(translationUpdater);
+        } else if (checkedId == R.id.updater_auto_button) {
+          parallaxLayout.setTranslationUpdater(new AnimatedTranslationUpdater(0.5f));
+        }
+      }
+    });
 
-        updaterGroup.check(R.id.updater_sensor_button);
+    updaterGroup.check(R.id.updater_sensor_button);
 
-        // Resets orientation when clicked
-        parallaxLayout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                translationUpdater.reset();
-            }
-        });
-    }
+    // Resets orientation when clicked
+    parallaxLayout.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        translationUpdater.reset();
+      }
+    });
+  }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        translationUpdater.registerSensorManager();
-    }
+  @Override
+  protected void onResume() {
+    super.onResume();
+    translationUpdater.registerSensorManager();
+  }
 
-    @Override
-    protected void onPause() {
-        super.onPause();
-        translationUpdater.unregisterSensorManager();
-    }
+  @Override
+  protected void onPause() {
+    super.onPause();
+    translationUpdater.unregisterSensorManager();
+  }
 }
